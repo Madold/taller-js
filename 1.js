5f48bd05d546cbd4ebbe51fc945096dd3b311242
Marco Portacio <marcoportacio4@gmail.com>
2023-10-15 16:34:53 -0500

    Se ha añadido la solucion de los ejercicios del 1 al 4

---
+++ 1.js
@@ -0,0 +1,11 @@
+//1. Determinar si un número es positivo o negativo o neutro
+
+var numero = 3;
+
+if (numero > 0) {
+    console.log("Positivo");
+} else if (numero < 0) {
+    console.log("Negativo");
+} else {
+    console.log("Neutral");
+}
---
+++ 3.js
@@ -0,0 +1,10 @@
+// 3.	Determinar si un número es divisible por 3 y 5
+
+var numero = 15;
+
+if (numero % 3 === 0 && numero % 5 === 0) {
+    console.log("El numero es divisible por 3 y 5");
+} else {
+    console.log("No es divisible por 3 y 5");
+}
+
